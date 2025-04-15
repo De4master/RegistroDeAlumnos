f2bcef9c1b7b8643d571ccf1c62986c2850ac491
Joao-Delacruz <svictoriopena@gmail.com>
2025-04-14 20:37:43 -0500

    .

--- app/database/conexion.py
+++ app/database/conexion.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Obtenemos la URL de la base de datos desde la variable de entorno
+# Obtenemos la URL de la base de datos desde la variable de entorn0
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:De4master12@localhost/registrodb")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
