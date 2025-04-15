ea8052a78d3d8e5999c2706efbebba627c95e342
Joao-Delacruz <svictoriopena@gmail.com>
2025-04-14 20:12:01 -0500

    Cambiando conexión a variable de entorno

--- app/database/conexion.py
+++ app/database/conexion.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL DATABASE
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:De4master12@localhost/registrodb"
+# Obtenemos la URL de la base de datos desde la variable de entorno
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:De4master12@localhost/registrodb")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
