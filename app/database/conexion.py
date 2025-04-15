0bd546b0fb979b275d89dc86332e03de8d049b39
Joao-Delacruz <svictoriopena@gmail.com>
2025-04-14 21:28:13 -0500

    solo modificando

--- app/database/conexion.py
+++ app/database/conexion.py
@@ -1,10 +1,9 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Obtenemos la URL de la base de datos desde la variable de entorn0
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:De4master12@localhost/registrodb")
+# URL DATABASE
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:De4master12@localhost/registrodb"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
